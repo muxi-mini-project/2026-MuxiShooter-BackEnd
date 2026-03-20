bc80bdc46d75b7ffe20c1715a430a334176a18fa
FireChickenMP4 <13930176445@163.com>
2026-03-20 23:02:16 +0800

    fix(JWT): tokens failed

--- src/middleware/middleware.go
+++ src/middleware/middleware.go
@@ -43,31 +43,7 @@ func JWTAuth(tokenParser JWTTokenParser, userRepository JWTUserRepository) gin.H
 			return
 		}
 
-		authHeader = strings.TrimSpace(authHeader)
-		var tokenStr string
-		tokenStr = authHeader
-		for {
-			if len(tokenStr) < 6 || !strings.EqualFold(tokenStr[:6], "Bearer") {
-				break
-			}
-			tokenStr = strings.TrimSpace(tokenStr[6:])
-			tokenStr = strings.TrimSpace(strings.TrimPrefix(tokenStr, ":"))
-		}
-
-		if tokenStr == "" {
-			c.AbortWithStatusJSON(http.StatusUnauthorized, dto.Response{
-				Code:    http.StatusUnauthorized,
-				Message: "Authorization格式错误",
-			})
-			return
-		}
-		if strings.Count(tokenStr, ".") != 2 {
-			c.AbortWithStatusJSON(http.StatusUnauthorized, dto.Response{
-				Code:    http.StatusUnauthorized,
-				Message: "token格式错误，请传入登录接口返回的token",
-			})
-			return
-		}
+		tokenStr := strings.TrimPrefix(authHeader, "Bearer ")
 
 		claims, err := tokenParser.ParseToken(tokenStr)
 		if err != nil {
